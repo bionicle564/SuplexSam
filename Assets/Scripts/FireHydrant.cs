b7363bf8d53abb31f6cbf0b08cd6f7576c25e63f
bionicle564 <57322673+bionicle564@users.noreply.github.com>
2026-02-08 02:14:37 -0500

    firehydrent

--- Assets/Scripts/FireHydrant.cs
+++ Assets/Scripts/FireHydrant.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 
 public class FireHydrant : HoldActions
 {
+    public GameObject waterBall;
+
+    private float timer;
+
+    public float sprayTime;
+
+    private void Start()
+    {
+        //timer = sprayTime;    
+    }
+
     private void Update()
     {
         if (held)
         {
-            print("fire held");
+            timer -= Time.deltaTime;
+
+            if(timer <= 0f)
+            {
+                GameObject temp = Instantiate(waterBall, this.transform.position + transform.forward *2f, transform.rotation);
+                temp.GetComponent<Rigidbody>().AddForce(transform.forward * 10,ForceMode.Impulse);
+                
+                timer = sprayTime;
+            }
         }
     }
 }
