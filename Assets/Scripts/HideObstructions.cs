9d4e95cf070fd0b6551ca8d9d0d619e9ad78f879
WCAF0137C <60712316+WCAF0137C@users.noreply.github.com>
2026-02-09 23:25:43 -0500

    Ground and ray tweaks

--- Assets/Scripts/HideObstructions.cs
+++ Assets/Scripts/HideObstructions.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 
 public class HideObstructions : MonoBehaviour
 {
-    //GameObject player;
+    GameObject player;
     public LayerMask layerMask;
 
     // Data structure for objects that need to be hidden
     // THIS IS DUMB AND STUPID AND we probably totally aren't going to change it BUT WE SHOULD
     public class ObjectToHide
     {
-        public ObjectToHide(GameObject obj, float t)
+        public ObjectToHide(GameObject obj, float t, bool i)
         {
             objectSelfReference = obj;
             time = t;
+            invis = i;
         }
 
         public GameObject objectSelfReference { get; set; }
         public float time { get; set; }
+        public bool invis { get; set; } // This checks whether the geometry is meant to be invisible
 
         public override string ToString() => $"({objectSelfReference.name}, {time})";
     }
@@ -28,17 +30,18 @@ public class HideObstructions : MonoBehaviour
 
     void Start()
     {
-        //player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        Ray ray = GetComponent<Camera>().ScreenPointToRay(Vector3.zero);
+        //Ray ray = GetComponent<Camera>().ScreenPointToRay(Vector3.zero);
+        Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
         {
-            Debug.Log("Hit?");
+            //Debug.Log("Hit?");
             if (hit.transform.gameObject.GetComponent<MeshRenderer>() != null)
             {
                 // Add it to the list of things to toggle off if it isn't already there
@@ -53,25 +56,27 @@ public class HideObstructions : MonoBehaviour
                 }
                 if (!inList)
                 {
-                    hideList.Add(new ObjectToHide(hit.transform.gameObject, 1.5f));
-                    if (hit.transform.gameObject.GetComponent<MeshRenderer>().enabled)
+                    bool isInvis = false;
+                    if (!hit.transform.gameObject.GetComponent<MeshRenderer>().enabled)
                     {
-                        hit.transform.gameObject.GetComponent<MeshRenderer>().enabled = false;
+                        isInvis = true;
                     }
+                    hit.transform.gameObject.GetComponent<MeshRenderer>().enabled = false;
+                    hideList.Add(new ObjectToHide(hit.transform.gameObject, 1.5f, isInvis));
                 }
             }
         }
 
         foreach (ObjectToHide obj in hideList)
         {
-            if (obj.objectSelfReference)
+            obj.time -= Time.deltaTime;
+            if (obj.time <= 0)
             {
-                obj.time -= Time.deltaTime;
-                if (obj.time <= 0)
+                if (!obj.invis)
                 {
                     obj.objectSelfReference.GetComponent<MeshRenderer>().enabled = true;
-                    hideList.Remove(obj);
                 }
+                hideList.Remove(obj);
             }
         }
     }
