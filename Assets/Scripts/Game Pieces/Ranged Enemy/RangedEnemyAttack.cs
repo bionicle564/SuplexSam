d99171bc736f5d493b492fdc0257574b70fe5146
WCAF0137C <60712316+WCAF0137C@users.noreply.github.com>
2026-03-06 19:21:06 -0500

    Update RangedEnemyAttack.cs

--- Assets/Scripts/Game Pieces/Ranged Enemy/RangedEnemyAttack.cs
+++ Assets/Scripts/Game Pieces/Ranged Enemy/RangedEnemyAttack.cs
@@ -1,22 +1,24 @@
 using UnityEngine;
+using UnityEngine.ProBuilder;
 
 public class RangedEnemyAttack : MonoBehaviour
 {
     [SerializeField] private float attackCooldown = 1f;
     [SerializeField] private float attackRange = 3f;
     [SerializeField] private Transform shootPoint;
+    [SerializeField] private Rigidbody bulletPrefab;
+    [SerializeField] private float bulletSpeed = 1f;
 
     private bool isGrabbed = false;
 
     public void OnGrabbed()
     {
-        isGrabbed = true;        // Enemy is now grabbed
-        //isAttacking = false;     // Cancel any attack in progress
+        isGrabbed = true; // Enemy is now grabbed
     }
 
     public void OnReleased()
     {
-        isGrabbed = false;       // Enemy released, can attack again
+        isGrabbed = false; // Enemy released, can attack again
     }
 
     private void Update()
@@ -32,6 +34,8 @@ public class RangedEnemyAttack : MonoBehaviour
         if (attackCooldown <= 0f && !isGrabbed)
         {
             Debug.Log("Shoot");
+            Rigidbody bullet = Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
+            bullet.linearVelocity = transform.forward * bulletSpeed;
         }
     }
 }
