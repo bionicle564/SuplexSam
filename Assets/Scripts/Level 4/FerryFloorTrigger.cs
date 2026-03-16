a4cc4ba55c0b6ebbdcc9e7ad30289eada8159391
bionicle564 <57322673+bionicle564@users.noreply.github.com>
2026-03-16 16:10:36 -0400

    level 4 script now handles 1 nested mesh

--- Assets/Scripts/Level 4/FerryFloorTrigger.cs
+++ Assets/Scripts/Level 4/FerryFloorTrigger.cs
@@ -15,6 +15,13 @@ public class FerryFloorTrigger : MonoBehaviour
             {
                 meshrenderers.Add(Level2Ground.transform.GetChild(i).GetComponent<MeshRenderer>());
             }
+            for(int j = 0;j<Level2Ground.transform.GetChild(i).transform.childCount; j++)
+            {
+                if (Level2Ground.transform.GetChild(i).GetChild(j).GetComponent<MeshRenderer>() != null)
+                {
+                    meshrenderers.Add(Level2Ground.transform.GetChild(i).GetChild(j).GetComponent<MeshRenderer>());
+                }
+            }
         }
     }
 
